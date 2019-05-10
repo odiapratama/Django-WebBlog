5ec981b9714b55f721dbab31310b8c71e9535c09
odiapratama <odiapratama@gmail.com>
2019-05-10 22:56:26 +0700

    initial commit

---
+++ articles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None, on_delete=None)
+
+    def __str__(self):
+        return self.title
+
+    def snippets(self):
+        return self.body[:100] + '...'
